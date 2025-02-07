6990a0b6c2b22da7ade506f62cfe7f082e829ae0
sean-le-tracy <97065885+sean-le-tracy@users.noreply.github.com>
2025-02-06 16:18:52 -0800

    test

---
+++ fread.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1
+
+int main() {
+    FILE *fp = fopen("/usr/lib/locale/locale-archive", "r");
+
+    if (fp == NULL) {
+        perror("Could not open file");
+        return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+    size_t bytesRead;
+
+    while (bytesRead = fread(buffer, 1, BUFFER_SIZE, fp) > 0){
+
+    }
+        
+
+    fclose(fp);
+    return 0;
+}
---
+++ read.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#define BUFFER_SIZE 8192
+
+int main(){
+    int fd = open ("(/usr/lib/locale/locale-archive", O_RDONLY);
+
+    if (fd == -1){
+        perror("Could not open file");
+        return 1;
+    }
+
+    ssize_t read_bytes;
+    char buffer[BUFFER_SIZE];
+
+    while(read_bytes = read(fd, buffer, BUFFER_SIZE) > 0){
+
+    }
+
+    if (read_bytes == -1){
+        perror("Could not read file");
+        return 1;
+    }
+    return 0;
+
+}
